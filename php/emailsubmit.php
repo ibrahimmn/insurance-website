7104193d63b1763b16397228216c1408c57d432f
the bob <ibrahim.mneimneh3@gmail.com>
2023-03-31 20:05:22 +0300

    contactus-start

---
+++ php/emailsubmit.php
@@ -0,0 +1,17 @@
+<?php
+
+
+if($_POST["message"]) {
+
+
+mail("ibrahimmneimneh2000@gmail.com", "Here is the subject line",
+
+
+$_POST["insert your message here"]. "From: an@email.address");
+
+
+}
+
+
+?>
+

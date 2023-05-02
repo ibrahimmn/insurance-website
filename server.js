7d3d7d3fc85b8db59841153c00860d077b2a4338
the bob <ibrahim.mneimneh3@gmail.com>
2023-05-02 13:44:34 +0300

    added auth0

--- server.js
+++ server.js
@@ -49,3 +49,30 @@ app.post('/', (req, res)=>{
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
 })
+
+
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 's%3Al3ozSdvQ83TtC9RvJ.CibaQoHtaY0H3QOB1kqR8H2A',
+  baseURL: 'http://localhost:5000',
+  clientID: 'hXHJzAtRz7iCU2Tw5kunwQgNsvdOQ8uz',
+  issuerBaseURL: 'https://dev-ygwg4eyha2gz1ahj.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+const { requiresAuth } = require('express-openid-connect');
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});

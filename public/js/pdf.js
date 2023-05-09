caf19ea83343917f04e01684127dfffa8e917fc8
the bob <ibrahim.mneimneh3@gmail.com>
2023-05-09 22:13:33 +0300

    getinsurance page updated

---
+++ public/js/pdf.js
@@ -0,0 +1,9 @@
+function show(){
+    document.getElementById("zero").style.display = "inline-block";
+    document.getElementById("aa").style.display = "inline-block";  
+  }
+  
+  function hide() {
+    document.getElementById("zero").style.display = "none";
+    document.getElementById("aa").style.display = "none";   
+  }
